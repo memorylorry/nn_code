c9ad09ddd940c929c5d42b0f4ef615706bf303c6
memorylorry <a214966>
2020-06-13 13:54:45 +0800

    整理索引方式

--- python_basic/numpy_basic/3_indexing.py
+++ python_basic/numpy_basic/3_indexing.py
@@ -1,6 +1,17 @@
 # 切片和常用的函数
 
-# P1 切片
+# 常规索引
+import numpy as np
+arr1:np.ndarray = np.arange(0,9,dtype=np.int)
+# P1.1 单个元素索引
+print('单个元素索引 : ' + arr1[2].__str__()) # 通过索引的方式引用
+print('单个元素索引 : ' + arr1[-1].__str__()) # 从后开始数的第1个数字
+
+# P1.2 索引数组方式索引
+x = np.arange(2,5)
+print('索引数组方式索引 : ' + arr1[x].__str__())
+
+# P1.3 切片方式索引
 '''
 切片操作基本表达式：object[start_index : end_index : step]
 
@@ -10,18 +21,12 @@ start_index：表示起始索引（包含该索引本身）；该参数省略时
 
 end_index：表示终止索引（不包含该索引本身）；该参数省略时，表示一直取到数据”端点“，至于是到”起点“还是到”终点“，同样由step参数的正负决定，step为正时直到”终点“，为负时直到”起点“。
 '''
-import numpy as np
-arr1:np.ndarray = np.arange(0,9,dtype=np.int)
+print('切片方式索引 : ' + arr1[2:4].__str__()) # 从2-4（不含4）的所有数字
+print('切片方式索引 : ' + arr1[-3:].__str__()) # 从倒数第3个开始到最后
 
-print(arr1[2]) # 通过索引的方式引用
-print(arr1[-1]) # 从后开始数的第1个数字
+print('切片方式索引 : ' + arr1[2:7:2].__str__()) #从2开始，到7，每次隔了2个步长
 
-print(arr1[2:4]) # 从2-4（不含4）的所有数字
-print(arr1[-3:]) # 从倒数第3个开始到最后
-
-print(arr1[2:7:2]) #从2开始，到7，每次隔了2个步长
-
-print(arr1[::-1]) # 逆序
+print('切片方式索引 : ' + arr1[::-1].__str__()) # 逆序
 
 arr2:np.ndarray = arr1[:] # 返回1个浅拷贝的列表出来，其实改变arr2的数据，arr1的也变！！！
 arr2[0] = 2
@@ -29,6 +34,24 @@ print('arr1 - ' + arr1.__str__())
 
 arr3:np.ndarray = arr2.copy() # 为了避免浅拷贝的问题，用copy可以做深拷贝！！！
 print('arr2 - ' + arr2.__str__())
+
+# P1.4 索引多维数组
+arr4:np.ndarray = np.arange(1,17).reshape(4,4)
+x = np.array([0,2])
+y = np.array([0,1])
+print('索引多维数组 arr4 : \n'+arr4.__str__())
+print(arr4[x,y]) # 分别用等长的x和y构成了索引的坐标；若这2者不等，则会异常
+
+# P1.5 布尔或“掩码”索引数组
+print('布尔或“掩码”索引数组 : \n'+arr4[arr4>10].__str__())
+
+# P1.6 将索引数组与标量结合（利用了广播机制）
+print('将索引数组与标量结合 : \n'+arr4[x,0].__str__()) # 这里的0会扩充成和x同样形状
+
+# P1.7 将索引数组与切片组合（利用了广播机制）
+print('将索引数组与切片组合 : \n'+arr4[x,0:3].__str__()) # 这你的0：3的每个数字都利用了广播特性；
+                                                     # 实际上，切片被转换为索引数组 np.array([[0,1,2]]) (shape (1,3))，
+                                                     # 它与索引数组一起广播以产生一个结果 shape(2,3) 的数组。
 
 
 # P2 常用取元素的函数
