d4535ddfc2a818d98b4fccbcd725a73ade4dde00
memorylorry <a214966>
2020-07-04 16:51:08 +0800

    目录调整

--- cv/pytorch_demo/11_none_linear_activations.py
+++ cv/pytorch_demo/11_none_linear_activations.py
@@ -6,5 +6,10 @@ import matplotlib.pyplot as plt
 # 使用非线性激励
 x = torch.arange(-10,10,1,dtype=torch.float).reshape(2,-1)
 x_r = F.elu(x)
+# print(x)
+# print(x_r)
+
+z=x.view(4,5)
+z[0][0]=999
+print(z)
 print(x)
-print(x_r)
