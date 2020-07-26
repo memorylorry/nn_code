90ac29f0ac7f067875efa92968204721ecd67025
memorylorry <a214966>
2020-07-26 17:38:19 +0800

    pandas csv reader

--- python_basic/pandas_basic/0_IO_tools.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df:pd.DataFrame = pd.read_csv('data_without_title.csv',sep=',',header=None,names=['date','open','high','close','low','volume','price_change'])
-
-print(df.index)
-print(df.columns)
-print(df['open'])
