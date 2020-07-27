367bd73f3c352996dfa4552f4645d744555bf008
memorylorry <a214966>
2020-07-27 19:43:09 +0800

    softmax 函数的使用

---
+++ cv/pytorch_demo/15_softmax_fun.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn.functional as F
+
+# 准备数据
+x = torch.tensor([
+    [1,3,8],
+    [3,7,5]
+],dtype=torch.float)
+
+# 使用 softmax 函数计算每个特征的概率
+y= F.softmax(x,dim=1)
+print(y)
+
+# 利用max可以取出某个轴的最大值
+print(torch.max(y,1))
